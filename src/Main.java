ce0f88e4916cf7b6e90a4d0edfb13648d365b92d
Jasmin Gelbmann <jasmin.gelbmann@gmail.com>
2021-02-15 10:43:00 +0100

    Processed

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,17 @@ public class Main {
 
         int luckyNumber = 32;
 
-        if (number%2 == 0) {
-            System.out.println("It's a round number");
+        if (number % 2 == 0) {
+            System.out.println("It's a even number");
         }
-        if (number%10 == 0) {
-            System.out.println("It's an even number");
+        if (number % 10 == 0) {
+            System.out.println("It's an round number");
         }
         if (number == luckyNumber) {
             System.out.println("It's the lucky number");
         }
-        if (number >= 10 && number < 100) {
-            System.out.println("It have two digits");
+        if (number >= 10 && number < 100 || number <= -10 && number > -100) {
+            System.out.println("It has two digits");
         }
 
     }
